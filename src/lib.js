ec4b0e42190f954eea5a00a69726d04d3e846dca
Toheeb Alade <aladetoheeb5@gmail.com>
2024-09-27 19:25:22 +0100

    Integration

---
+++ src/lib.js
@@ -0,0 +1,22 @@
+import { Account, BN } from 'fuels';
+
+export const CURRENT_ENVIRONMENT = process.env.VITE_DAPP_ENVIRONMENT
+
+// The node URL is determined by the current environment too.
+export const NODE_URL ='https://testnet.fuel.network/v1/graphql';
+
+// export interface AppWallet {
+//   wallet?: Account;
+//   walletBalance?: BN;
+//   refreshWalletBalance?: () => Promise<void>;
+// }
+
+export const TESTNET_FAUCET_LINK = 'https://faucet-testnet.fuel.network/';
+
+export const FAUCET_LINK = TESTNET_FAUCET_LINK;
+
+export const FAUCET_PRIVATE_KEY = '0x01';
+
+export const DOCS_URL = 'https://docs.fuel.network';
+
+export const TESTNET_CONTRACT_ID = process.env.VITE_TESTNET_CONTRACT_ID;
